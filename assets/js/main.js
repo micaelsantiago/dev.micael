2197b99249a2997a6402fbcf3972c4aa770c578e
Micael Santiago <83787168+micaelsantiago@users.noreply.github.com>
2023-12-25 01:21:23 -0300

    Header completo

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const toggleMenuMobile = () => {
+  document.querySelector('.menu-items').classList.toggle('active');
+  document.querySelector('.menu-mobile .row1').classList.toggle('active');
+  document.querySelector('.menu-mobile .row2').classList.toggle('active');
+  document.querySelector('.menu-mobile .row3').classList.toggle('active');
+}
